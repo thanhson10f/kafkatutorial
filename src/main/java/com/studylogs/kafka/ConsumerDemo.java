dfdfd2cc4f96f00865500f1a7b76b8157e04458c
Son Nguyen - Company <ntson.sh@gmail.com>
2019-03-27 09:58:53 +0700

    Finish ConsumerDemo

--- src/main/java/com/studylogs/kafka/ConsumerDemo.java
+++ src/main/java/com/studylogs/kafka/ConsumerDemo.java
@@ -1,9 +1,15 @@
 package com.studylogs.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 
 public class ConsumerDemo {
@@ -11,8 +17,31 @@ public class ConsumerDemo {
         final Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
 
         String bootstrapServers = "localhost.localdomain:9092";
+        String groupId = "my_second_app";
+        String topic = "first_topic";
 
+        // Create consumer configs
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // Subscribe consumer to our topic(s)
+        consumer.subscribe(Collections.singleton(topic));
+
+        // poll for new data
+        while (true){
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for(ConsumerRecord<String, String> record : records){
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: "+record.partition()+", Offset: "+record.offset());
+            }
+        }
     }
 }
